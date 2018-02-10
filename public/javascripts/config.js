7e213d6627d8a9da1ae2bf32a0e5f2618a3242b5
AleitaT <traina@oregonstate.edu>
2018-02-10 10:21:56 -0800

    config

--- public/javascripts/config.js
+++ public/javascripts/config.js
@@ -1,4 +1,4 @@
 exports.port = process.env.PORT || 3000
 exports.GOOGLE_CLIENT_ID = process.env.GOOGLE_API_CLIENT_ID
 exports.GOOGLE_CLIENT_SECRET = process.env.GOOGLE_API_CLIENT_SECRET
-exports.REDIRECT_URI="http://dev.alietatrain.com:3000/oauth2callback"
+exports.REDIRECT_URI=process.env.REDIRECT_URI
